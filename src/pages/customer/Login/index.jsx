745106705f1cadca4efbf0be1ecbdc49db373e6e
KhoaLVD <khoalvd2412@gmail.com>
2024-11-29 02:02:05 +0700

    update admin

--- src/pages/customer/Login/index.jsx
+++ src/pages/customer/Login/index.jsx
@@ -1,3 +1,63 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-    return <div>Login Page</div>;
+    return (
+        <section className="bg-primary">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label
+                                    htmlFor="username"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Tài khoản
+                                </label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="nguyenvana"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Mật khẩu
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    required
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-primary bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            >
+                                Đăng nhập
+                            </button>
+                            <p className="text-sm font-light text-gray-500">
+                                Bạn chưa có tài khoản?{" "}
+                                <Link
+                                    to="/register"
+                                    className="font-medium text-primary-600 hover:underline"
+                                >
+                                    Tạo tài khoản mới tại đây
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/pages/customer/Register/index.jsx
+++ src/pages/customer/Register/index.jsx
@@ -1,3 +1,123 @@
+import { Link } from "react-router-dom";
+
 export default function Register() {
-    return <div>Register Page</div>;
+    return (
+        <section className="bg-primary">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-secondary md:text-2xl">
+                            Tạo tài khoản
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label
+                                    htmlFor="username"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Tài khoản
+                                </label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="nguyenvana"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Mật khẩu
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="name@company.com"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="phone-number"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Số điện thoại
+                                </label>
+                                <input
+                                    type="text"
+                                    name="phone-number"
+                                    id="phone-number"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="0903123456"
+                                    required
+                                />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input
+                                        id="terms"
+                                        aria-describedby="terms"
+                                        type="checkbox"
+                                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                                        required
+                                    />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label
+                                        htmlFor="terms"
+                                        className="font-light text-gray-500"
+                                    >
+                                        Tôi chấp nhận các{" "}
+                                        <a
+                                            className="font-medium text-primary-600 hover:underline"
+                                            href="#"
+                                        >
+                                            Điều khoản và Điều kiện
+                                        </a>
+                                    </label>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-primary bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                            >
+                                Tạo tài khoản
+                            </button>
+                            <p className="text-sm font-light text-gray-500">
+                                Bạn đã có tài khoản?{" "}
+                                <Link
+                                    to="/login"
+                                    className="font-medium text-primary-600 hover:underline"
+                                >
+                                    Đăng nhập tại đây
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
