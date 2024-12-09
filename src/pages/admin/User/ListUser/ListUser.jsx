c6cd6a9db1869765b67b07bd3d4e348b5f7cf30a
KhoaLVD <khoalvd2412@gmail.com>
2024-12-09 17:30:07 +0700

    update user

--- src/pages/admin/User/ListUser/ListUser.jsx
+++ src/pages/admin/User/ListUser/ListUser.jsx
@@ -1,7 +1,8 @@
 import { fetchListUsers } from "./reducer"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
-import { deleteUser } from "../DeleteUser/reducer"
+
+import UserDetail from "./UserDetail"
 
 export default function ListUser() {
     const dispatch = useDispatch()
@@ -25,33 +26,7 @@ export default function ListUser() {
     const renderListUsers = () =>{
         const {data} = props;
         if(data && data.length > 0){
-            return data.map((user)=>{
-                return (  
-                <tr key={user.taiKhoan} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {user.taiKhoan}
-                    </th>
-                    <td className="px-6 py-4">
-                    {user.hoTen}
-                    </td>
-                    <td className="px-6 py-4">
-                    {user.email}
-                    </td>
-                    <td className="px-6 py-4">
-                    {user.soDT}
-                    </td>
-                    <td className="px-6 py-4">
-                    {user.maLoaiNguoiDung}
-                    </td>
-                    <td className="px-6 py-4">
-                    <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Chỉnh sửa</button>
-                    <button type="button" 
-                    onClick={() => {dispatch(deleteUser(user.taiKhoan))}}
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Xoá</button>
-                    </td>
-                </tr>
-                )
-            })
+            return data.map((user)=><UserDetail key={user.taiKhoan} user={user} />)
         }
     }
 
--- src/utils/redux/reducers/adminReducer.js
+++ src/utils/redux/reducers/adminReducer.js
@@ -2,5 +2,6 @@ import adminLoginReducer from "@/pages/admin/Auth/duck/reducer";
 import adminListUserReducer from "@/pages/admin/User/ListUser/reducer";
 import adminAddUserReducer from "@/pages/admin/User/AddUser/reducer";
 import adminDeleteUserReducer from "@/pages/admin/User/DeleteUser/reducer";
+import adminUpdateUserReducer from "@/pages/admin/User/UpdateUser/reducer";
 
-export default { adminLoginReducer, adminListUserReducer, adminAddUserReducer, adminDeleteUserReducer };
+export default { adminLoginReducer, adminListUserReducer, adminAddUserReducer, adminDeleteUserReducer, adminUpdateUserReducer };
